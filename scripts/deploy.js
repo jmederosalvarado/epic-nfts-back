0fefaa8f38414c39d5705df1d352c757e77cfe93
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2021-10-01 23:12:10 -0400

    Just to keep track of changes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,15 +4,9 @@ const main = async () => {
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
 
-  // Call the function.
-  let txn = await nftContract.makeAnEpicNFT();
-  // Wait for it to be mined.
-  await txn.wait();
-
-  // Mint another NFT for fun.
-  txn = await nftContract.makeAnEpicNFT();
-  // Wait for it to be mined.
-  await txn.wait();
+  let txn = await nftContract.makeAnEpicNFT(); // Call the function.
+  await txn.wait(); // Wait for it to be mined.
+  console.log("Minted NFT #1");
 };
 
 const runMain = async () => {
